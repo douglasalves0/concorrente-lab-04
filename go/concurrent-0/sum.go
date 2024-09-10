42c56708ca0c4c159e23486d35ceef01f9e4cf5d
121111728 DOUGLAS ALVES DE SOUSA <douglas.sousa@lcc3-090.lcc.ufcg.edu.br>
2024-09-10 14:54:23 -0300

    problematic code

---
+++ go/concurrent-0/sum.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// read a file from a filepath and return a slice of bytes
+func readFile(filePath string) ([]byte, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("Error reading file %s: %v", filePath, err)
+		return nil, err
+	}
+	return data, nil
+}
+
+// sum all bytes of a file
+func sum(filePath string, c chan int) {
+	data, _ := readFile(filePath)
+
+	_sum := 0
+	for _, b := range data {
+		_sum += int(b)
+	}
+
+	c <- _sum
+}
+
+// print the totalSum for all files and the files with equal sum
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <file1> <file2> ...")
+		return
+	}
+
+	var totalSum int64
+	sums := make(map[int][]string)
+	c := make(chan int)
+	for _, path := range os.Args[1:] {
+		go sum(path, c)
+	}
+	for _, path := range os.Args[1:] {
+		_sum := <-c
+
+		totalSum += int64(_sum)
+
+		sums[_sum] = append(sums[_sum], path)
+	}
+
+	fmt.Println(totalSum)
+
+	for sum, files := range sums {
+		if len(files) > 1 {
+			fmt.Printf("Sum %d: %v\n", sum, files)
+		}
+	}
+}
